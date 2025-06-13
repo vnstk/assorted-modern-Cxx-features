df404b656cf91316b5975d9abd3ced99cfdb3750
vnstk <75968083+vnstk@users.noreply.github.com>
2025-06-13 17:20:59 -0400

    std::span isn't all it's cracked up to be.

--- ctainer-views.cpp
+++ ctainer-views.cpp
@@ -3,52 +3,107 @@
 #include <array>
 #include <initializer_list>
 #include <string>
+#include <string_view>
 #include <vector>
+
+/* Refresh
+terms:
+		**rank** is how many dimensions
+				static_assert(std::rank<int[][5][7]>{} == 3);
+, and
+		**extent** is # of elems in a particular dimension
+				static_assert(std::extent<int[][5][7], 2>{} == 7);  
+*/
 
 #ifdef VER_ge20
 #	include <span>
+
+void test__span ()
+{	PRenteredFU;
+	double seqA[7]; // 1-D plain arr
+	double seqB[31][7]; // 2-D plain arr
+	double seqC[0][7]; // 2-D plain arr, with ridiclous extent of 1st dim
+	double seqD[7][0]; // 2-D plain arr, with ridiclous extent of last dim.
+
+	// Multidimensional array must have bounds for all dimensions except the first
+/*
+#if 0 // Kinda interesting:
+	double seqZ[][7];         <=== won't compile
+	double seqZ[][7] = {};    <=== compiles.
+	extern double seqZ[][7];  <=== compiles.
 #endif
+*/
+	double seqE[][7] = {};
+	extern double seqF[][7];
+
+	double *seqG; // will it work?
+
+	std::array<double,7> seqH;
+	std::vector<float> seqI;
+	std::vector<float> seqJ(7 /*# of elems to be default-initialized*/);
+
+	std::vector<float> seqP{2.2F,4.4F,6.6F};
+	// Note, this is the specially-defd-in-std bool specializat of vec.
+	std::vector<bool> seqQ{true,false,true,true,false};
+	// Why is a span of initializer_list said to have dynamic extent?  Why isn't initLi.size() constexpr?
+	const std::initializer_list<double> seqS{3.3,1.1,2.2,7.7,5.5,4.4,6.6};
+	const std::string seqT{"bunnies"};
+	const std::string_view seqU{seqT};
+
+	std::span spaS_another{seqS};
+	std::span spaY = spaS_another.subspan(1,3);
+	std::span spaZ = spaS_another.subspan(1,std::dynamic_extent);
+
+#define ABOUTspan(sp) \
+	printf("(Ln%d) \e[32m%6s\e[0m  empty:%c %3zu=size,i.e.#elems "\
+			"%2zu=sizeof(T)  %4zu=size_bytes  extent:%s\n",     \
+	       __LINE__, #sp, sp.empty()?'Y':'N', sp.size(),   \
+			sizeof(decltype(sp)::element_type), sp.size_bytes(),  \
+	       (sp.extent==std::dynamic_extent) ? "dyn" : (std::to_string(sp.extent).c_str()))
+
+#define CHKspan(ident)                     \
+	std::span spa ## ident{seq ## ident};   \
+	ABOUTspan(spa ## ident)
+
+	CHKspan(A);
+	CHKspan(B);
+//	CHKspan(C);   Multidim array, extent of whose 1st dim is 0.
+//	CHKspan(D);   Multidim array, extent of whose last dim is 0.
+//	CHKspan(E);   Multidim array, extent of whose 1st dim is unspecified.
+//	CHKspan(F);   Multidim extern array, extent of whose 1st dim is unspecified.
+//	CHKspan(G);   Just "float*", which of course is same as "float[]".
+	CHKspan(H);
+	CHKspan(I);
+	CHKspan(J);
+	CHKspan(P);
+//	CHKspan(Q);   The bool specializations of vector; span doesn't like it!
+	CHKspan(S);
+	CHKspan(T);
+	CHKspan(U);
+	ABOUTspan(spaY); // On a static-extent subspan?
+	ABOUTspan(spaZ); // On a dynamic-extent subspan?
+#undef CHKspan
+#undef ABOUTspan
+
+	/* Now this is interesting: normally we'd get segfault just
+	when initializing charNoneSuch and floatNoneSuch; but here,
+	segfault doesn't occur until+unless we *use* those bogus values. */
+	char charNoneSuch = spaT[33333];
+	float floatNoneSuch = spaP[33333];
+if ((uintptr_t)&test__span == 0) // (So that the next line doesn't run.)
+	PRmsg("charNoneSuch=%c floatNoneSuch=%f\n", charNoneSuch,floatNoneSuch);
+	
+}
+#endif // VER_ge20
+/*
+	Turns out, std::span is incompatible with quite a few kinds of sequences!!
+*/
+
 
 #if defined(VER_ge23) || defined(__cpp_lib_mdspan)
 #	include <mdspan>
 //
 #endif
-
-void test__span ()
-{	PRenteredFU;
-
-	float c_arr_1d[7];
-	float c_arr_2d[31][7];
-	std::array<float,7> arr00;
-	std::vector<float> vec00;
-	std::vector<float> vec01(7);
-	const std::vector<float> vec02(7);
-	const std::vector<float> vec03{3.3F,1.1F,2.2F,7.7F,5.5F,4.4F,6.6F};
-	std::vector<bool> vec04{true,false,true,true,false};
-	const std::initializer_list<float> ili{3.3F,1.1F,2.2F,7.7F,5.5F,4.4F,6.6F};
-	const std::string str00{"bunnies"};
-
-#ifdef VER_ge20
-
-#define ABOUTspan(sp) \
-	printf("(Ln%d)  " #sp " --- %3zu=size %4zu=size_bytes empty:%c extent=%s\n",     \
-	       __LINE__, sp.size(), sp.size_bytes(), sp.empty()?'Y':'N',         \
-	       (sp.extent==std::dynamic_extent) ? "dyn" : (std::to_string(sp.extent).c_str()))
-
-	std::span sp00(c_arr_1d);		ABOUTspan(sp00);
-	std::span sp01(c_arr_2d);		ABOUTspan(sp01);
-	std::span sp02(arr00);			ABOUTspan(sp02);
-	std::span sp03(vec00);			ABOUTspan(sp03);
-	vec00.insert(/*before pos*/ vec00.cend(), {11.1F,99.9F,0.003F});
-	                      			ABOUTspan(sp03); // Answers *not* updated!!
-	std::span sp03b(vec00);			ABOUTspan(sp03b);
-	std::span sp04(vec01);			ABOUTspan(sp04);
-	std::span sp05(vec02);			ABOUTspan(sp05);
-	std::span sp06(vec03);			ABOUTspan(sp05);
-	std::span sp07(ili);			ABOUTspan(sp06);
-	std::span sp08(str00);			ABOUTspan(sp07);
-#endif
-}
 
 void test__mdspan ()
 {	PRenteredFU;
@@ -60,17 +115,7 @@ void test__mdspan ()
 
 int main ()
 {
+#if VER_ge20
 	test__span();
-}
-
-#if 0
-span
-(C++20)
-a non-owning view over a contiguous sequence of objects
-(class template)
-
-mdspan
-(C++23)
-a multi-dimensional non-owning array view
-
 #endif
+}
